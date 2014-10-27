8288df9f781ad902dd5b67868f0e89cc5d378d53
Josh Curtiss <J.Curtiss@RockValleyCollege.edu>
2014-10-27 16:06:02 -0500

    Updated My Task List to fully implement Core Data
    Add/Remove/Load all talk to Core Data now. Furthermore, Core Data was implemented fully in the TaskManager object, so that the view controller doesn't have to be directly aware of the model.
    
    It's not done the most efficient way possible. After any change is made (add/remove), it reloads from the database. However, it keeps the code simple. The results are stored in an array, and the getTask() and getTaskCount() methods just reference the entities in the array.

--- My Task List/My Task List/TaskManager.swift
+++ My Task List/My Task List/TaskManager.swift
@@ -7,28 +7,48 @@
 //
 
 import UIKit
+import CoreData
 
 var taskMgr:TaskManager = TaskManager()
 
-struct task {
-    var name="Unnamed"
-    var desc="Unknown"
-}
-
 class TaskManager:NSObject
 {
-    var tasks=[task]()
+    var tasks:NSArray!
+    var context:NSManagedObjectContext!
+    override init()
+    {
+        super.init()
+        var appDel:AppDelegate=UIApplication.sharedApplication().delegate as AppDelegate
+        context=appDel.managedObjectContext
+        load()
+    }
+    func load()
+    {
+        var req=NSFetchRequest(entityName: "Task")
+        req.returnsObjectsAsFaults=false
+        var results:NSArray=context.executeFetchRequest(req, error: nil)!
+        tasks=context.executeFetchRequest(req, error: nil)!
+    }
     func addTask(name:String,desc:String)
     {
-        tasks.append(task(name: name, desc: desc))
+        var newTask=NSEntityDescription.insertNewObjectForEntityForName("Task", inManagedObjectContext: context) as NSManagedObject
+        newTask.setValue(name, forKey: "name")
+        newTask.setValue(desc, forKey: "desc")
+        context.save(nil)
+        load()
     }
     func removeTask(index:Int)
     {
-        tasks.removeAtIndex(index)
+        context.deleteObject(tasks[index] as NSManagedObject)
+        context.save(nil)
+        load()
     }
     func getTask(index:Int)->(name:String,desc:String)
     {
-        return (tasks[index].name,tasks[index].desc)
+        var t:NSManagedObject=tasks[index] as NSManagedObject
+        var name:String=t.valueForKey("name") as String
+        var desc:String=t.valueForKey("desc") as String
+        return (name,desc)
     }
     func getTaskCount()->Int
     {
