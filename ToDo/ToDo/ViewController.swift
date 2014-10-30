8b4fde33682abb20a5a0af58fa8781e5da75466f
Josh Curtiss <git@curtiss.me>
2014-10-30 13:55:15 -0500

    Added ToDo app for learning.
    This is based on a tutorial from ios-blog.co.uk named, “Developing iOS8
    Apps Using Swift – Create a To-Do Application”.
    
    http://ios-blog.co.uk/tutorials/developing-ios8-apps-using-swift-create-
    a-to-do-application/
    
    The app isn’t written yet, I’ll commit each step along the way. All
    I’ve done is set up the first textfield element.

---
+++ ToDo/ToDo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ToDo
+//
+//  Created by Joshua Curtiss on 10/29/14.
+//  Copyright (c) 2014 Joshua Curtiss. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
