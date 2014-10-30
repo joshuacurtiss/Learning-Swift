dde33bdd19d85a2593225db4ef31cebf1872f494
Josh Curtiss <git@curtiss.me>
2014-10-30 14:39:18 -0500

    Added custom table view controller.
    Should be able to navigate between the two views now, however, the Cancel and Done buttons aren't working as promised.

---
+++ ToDo/ToDo/ToDoListTableViewController.h
@@ -0,0 +1,17 @@
+//
+//  ToDoListTableViewController.h
+//  ToDo
+//
+//  Created by Joshua Curtiss on 10/30/14.
+//  Copyright (c) 2014 Joshua Curtiss. All rights reserved.
+//
+
+#ifndef ToDo_ToDoListTableViewController_h
+#define ToDo_ToDoListTableViewController_h
+#endif
+
+@interface ToDoListTableViewController (Workaround)
+
+-(IBAction)unwindToList: (UIStoryboardSegue *)segue;
+
+@end
